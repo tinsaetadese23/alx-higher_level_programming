26c36da11a18a673a3314178b94e7ba8f1e79973
Tinsae Tadese <104816113+tinsae-tadese@users.noreply.github.com>
2023-10-24 21:07:45 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Creates a class Square
+"""
+
+
+class Square:
+    """ Square Class
+    """
+    def __init__(self, size):
+        """
+        Constructructor with instantiation
+        of size
+        """
+        self.__size = size
